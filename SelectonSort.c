211e062fa4a45334d76c2fe0b959894460cbd087
heynameisabhi <169142129+heynameisabhi@users.noreply.github.com>
2025-04-20 18:16:37 +0530

    Update SelectonSort.c

--- SelectonSort.c
+++ SelectonSort.c
@@ -1,33 +1,45 @@
-#include<stdio.h>
-#include<time.h>
-void main(){
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int i,j,n;
+int main() {
+    int i, j, n, k, temp;
 
-printf("Enter the size array\n");
-scanf("%d",&n);
-int temp,a[n],k;
-srand(time(NULL));
-for(i=0;i<n;i++){
- a[i]=rand()%25000;
-}
-clock_t start = clock();
-for(i=0;i<n;i++){
-j=i;
- for(k=i+1;k<n;k++){
-    if(a[k]<a[j]){
-       j=k;
-      }
-}
-  temp = a[i];
-  a[i]=a[j];
-  a[j]=temp;
-}
-clock_t end = clock();
-double timetaken = (double)(end-start)/CLOCKS_PER_SEC;
-printf("the time taken = %F in sec",timetaken);
-/*for(i=0;i<n;i++){
- printf("%d ",a[i]);
-}*/
-printf("\n");
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int a[n]; // Or use malloc for portability
+    srand(time(NULL));
+
+    for(i = 0; i < n; i++) {
+        a[i] = rand() % 25000;
+    }
+
+    clock_t start = clock();
+
+    for(i = 0; i < n - 1; i++) {
+        j = i;
+        for(k = i + 1; k < n; k++) {
+            if(a[k] < a[j]) {
+                j = k;
+            }
+        }
+        temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    clock_t end = clock();
+    double timetaken = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("The time taken = %.6f seconds\n", timetaken);
+
+    // Optional: Print the sorted array
+    /*
+    for(i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    */
+
+    return 0;
 }
